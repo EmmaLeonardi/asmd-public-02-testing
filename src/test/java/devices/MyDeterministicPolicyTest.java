ee05e77a742b4b995203e56e98ecca491356c6c2
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2026-02-27 12:42:15 +0100

    Renamed Deterministic Device into Deterministic Policy tests

--- src/test/java/devices/MyDeterministicPolicyTest.java
+++ src/test/java/devices/MyDeterministicPolicyTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MyDeterministicDeviceTest {
+public class MyDeterministicPolicyTest {
 
     public FailingPolicy policyFalse;
     public FailingPolicy policyTrue;
